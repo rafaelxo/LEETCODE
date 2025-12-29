46ce9db354abf59513de312cdd9d61822ecc7a4a
rafaelxo <rafaelxo2007@gmail.com>
2025-12-29 14:07:31 -0300

    exercicio 3

---
+++ exercicio3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class exercicio3 {
+    public int lengthOfLongestSubstring(String s) {
+        boolean[] arr = new boolean[128];
+        int resp = 0, r = 0, l = 0;
+        while (r < s.length()) {
+            if (!arr[s.charAt(r)]) {
+                arr[s.charAt(r)] = true;
+                resp = Math.max(resp, r - l + 1);
+                r++;
+            } else {
+                arr[s.charAt(l)] = false;
+                l++;
+            }
+        }
+        return resp;
+    }
+}
