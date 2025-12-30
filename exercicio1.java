de9244a30624667136a77cf047fb00401c2879cb
rafaelxo <rafaelxo2007@gmail.com>
2025-12-30 03:23:01 -0300

    exercicio 7

--- exercicio1.java
+++ exercicio1.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class exercicio1 {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> resp = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if (resp.containsKey(complement)) return new int[] { resp.get(complement), i };
-            resp.put(nums[i], i);
+            if (map.containsKey(complement)) return new int[] { map.get(complement), i };
+            map.put(nums[i], i);
         }
         return new int[] {};
     }
--- exercicio3.java
+++ exercicio3.java
@@ -1,16 +1,15 @@
+import java.util.Arrays;
+
 class exercicio3 {
     public int lengthOfLongestSubstring(String s) {
-        boolean[] arr = new boolean[128];
-        int resp = 0, r = 0, l = 0;
-        while (r < s.length()) {
-            if (!arr[s.charAt(r)]) {
-                arr[s.charAt(r)] = true;
-                resp = Math.max(resp, r - l + 1);
-                r++;
-            } else {
-                arr[s.charAt(l)] = false;
-                l++;
-            }
+        int[] last = new int[128];
+        Arrays.fill(last, -1);
+        int resp = 0;
+        int i = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (last[s.charAt(j)] >= i) i = last[s.charAt(j)] + 1;
+            last[s.charAt(j)] = j;
+            resp = Math.max(resp, j - i + 1);
         }
         return resp;
     }
