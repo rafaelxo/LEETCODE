cda5614f51cadfc46c9b3ef071a2322f171fc868
rafaelxo <rafaelxo2007@gmail.com>
2026-01-01 22:19:38 -0300

    att

---
+++ exercicio10.c
@@ -0,0 +1,24 @@
+#include <stdbool.h>
+
+bool isMatch(char *s, char *p) {
+    int sLen = strlen(s);
+    int pLen = strlen(p);
+    bool dp[sLen + 1][pLen + 1];
+    for (int i = 0; i <= sLen; i++) {
+        for (int j = 0; j <= pLen; j++) dp[i][j] = false;
+    }
+    dp[0][0] = true;
+    for (int j = 2; j <= pLen; j++) {
+        if (p[j - 1] == '*') dp[0][j] = dp[0][j - 2];
+    }
+    for (int i = 1; i <= sLen; i++) {
+        for (int j = 1; j <= pLen; j++) {
+            if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) dp[i][j] = dp[i - 1][j - 1];
+            else if (p[j - 1] == '*') {
+                dp[i][j] = dp[i][j - 2];
+                if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) dp[i][j] = dp[i][j] || dp[i - 1][j];
+            }
+        }
+    }
+    return dp[sLen][pLen];
+}
