96a22b0018e03f261a600bb3e2f80ac93f6fce34
rafaelxo <rafaelxo2007@gmail.com>
2026-01-06 01:17:15 -0300

    exercicio 21

---
+++ exercicio21.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode resp = new ListNode(0);
+        ListNode curr = resp;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = (list1 != null) ? list1 : list2;
+        return resp.next;
+    }
+}
