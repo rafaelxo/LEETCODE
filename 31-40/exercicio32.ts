265707199121a7206826574c5a93676855685718
rafaelxo <rafaelxo2007@gmail.com>
2026-01-10 01:45:29 -0300

    exercicio 32

---
+++ 31-40/exercicio32.ts
@@ -0,0 +1,14 @@
+function longestValidParentheses(s: string): number {
+    let maxLen = 0;
+    const stack: number[] = [];
+    stack.push(-1);
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') stack.push(i);
+        else {
+            stack.pop();
+            if (stack.length === 0) stack.push(i);
+            else maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+        }
+    }
+    return maxLen;
+};
