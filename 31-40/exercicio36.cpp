ef202129f9366effee835173192d029bcfe8bc7d
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-01-11 03:13:12 -0300

    exercicio 36

---
+++ 31-40/exercicio36.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool row[9][9] = {false};
+        bool col[9][9] = {false};
+        bool box[9][9] = {false};
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                int num = board[i][j] - '1';
+                int b = (i / 3) * 3 + (j / 3);
+                if (row[i][num] || col[j][num] || box[b][num]) return false;
+                row[i][num] = col[j][num] = box[b][num] = true;
+            }
+        }
+        return true;
+    }
+};
