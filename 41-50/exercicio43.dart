9845b6d1d0ead7046a975cfd304f67b190db7e96
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-01-15 02:17:20 -0300

    exercicio 43

--- 41-50/exercicio43.dart
+++ 41-50/exercicio43.dart
@@ -2,20 +2,20 @@ class Solution {
     String multiply(String num1, String num2) {
         if (num1 == "0" || num2 == "0") return "0";
         int n = num1.length, m = num2.length;
-        List<int> res = List.filled(n + m, 0);
+        List<int> aux = List.filled(n + m, 0);
         for (int i = n - 1; i >= 0; i--) {
             int d1 = num1.codeUnitAt(i) - 48;
             for (int j = m - 1; j >= 0; j--) {
                 int d2 = num2.codeUnitAt(j) - 48;
-                int sum = d1 * d2 + res[i + j + 1];
-                res[i + j + 1] = sum % 10;
-                res[i + j] += sum ~/ 10;
+                int sum = d1 * d2 + aux[i + j + 1];
+                aux[i + j + 1] = sum % 10;
+                aux[i + j] += sum ~/ 10;
             }
         }
-        StringBuffer sb = StringBuffer();
+        StringBuffer resp = StringBuffer();
         int i = 0;
-        while (i < res.length && res[i] == 0)i++;
-        for (; i < res.length; i++) sb.write(res[i]);
-        return sb.toString();
+        while (i < aux.length && aux[i] == 0)i++;
+        for (; i < aux.length; i++) resp.write(aux[i]);
+        return resp.toString();
     }
 }
