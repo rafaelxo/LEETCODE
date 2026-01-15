d28556f8aac9008bb1d1439ee58e1052a0ed511e
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-01-15 02:20:34 -0300

    exercicio 43

--- 41-50/exercicio43.dart
+++ 41-50/exercicio43.dart
@@ -4,17 +4,17 @@ class Solution {
         int n = num1.length, m = num2.length;
         List<int> aux = List.filled(n + m, 0);
         for (int i = n - 1; i >= 0; i--) {
-            int d1 = num1.codeUnitAt(i) - 48;
+            int c1 = num1.codeUnitAt(i) - 48;
             for (int j = m - 1; j >= 0; j--) {
-                int d2 = num2.codeUnitAt(j) - 48;
-                int sum = d1 * d2 + aux[i + j + 1];
+                int c2 = num2.codeUnitAt(j) - 48;
+                int sum = c1 * c2 + aux[i + j + 1];
                 aux[i + j + 1] = sum % 10;
                 aux[i + j] += sum ~/ 10;
             }
         }
         StringBuffer resp = StringBuffer();
         int i = 0;
-        while (i < aux.length && aux[i] == 0)i++;
+        while (i < aux.length && aux[i] == 0) i++;
         for (; i < aux.length; i++) resp.write(aux[i]);
         return resp.toString();
     }
