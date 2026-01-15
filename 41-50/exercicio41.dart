3731a2d909fe3b0c9094f9d54897099df60ff202
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-01-15 01:31:41 -0300

    exercicio 41

--- 41-50/exercicio41.dart
+++ 41-50/exercicio41.dart
@@ -1,5 +1,20 @@
 class Solution {
     int firstMissingPositive(List<int> nums) {
-
+        int n = nums.length;
+        int i = 0;
+        while (i < n) {
+            int idx = nums[i] - 1;
+            if (nums[i] > 0 &&
+                nums[i] <= n &&
+                nums[i] != nums[idx]) {
+                int temp = nums[i];
+                nums[i] = nums[idx];
+                nums[idx] = temp;
+            } else i++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
     }
 }
