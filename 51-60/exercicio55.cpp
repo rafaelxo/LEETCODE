3ff829d8e3734eb80c0b4501b61794e301154194
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-01-23 00:56:54 -0300

    exercicio 55

---
+++ 51-60/exercicio55.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxR = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i > maxR) return false;
+            maxR = max(maxR, i + nums[i]);
+            if (maxR >= n - 1) return true;
+        }
+        return true;
+    }
+};
