f30772ee9e0aef4068d8e5673243fd245cb85dfb
rafaelxo <rafaelxo2007@gmail.com>
2026-01-25 21:32:50 -0300

    exercicio 58

--- 51-60/exercicio58.java
+++ 51-60/exercicio58.java
@@ -1,8 +1,8 @@
 class exercicio58 {
     public int lengthOfLastWord(String s) {
         int i = s.length() - 1, resp = 0;
-        while (i >= 0 && s.charAt(i) == " ") i--;
-        while (i >= 0 && s.charAt(i) != " ") {
+        while (i >= 0 && s.charAt(i) == ' ') i--;
+        while (i >= 0 && s.charAt(i) != ' ') {
             resp++;
             i--;
         }
