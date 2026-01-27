aceae4bd2da1ccd9cc2ee8169343e4a047466eb7
rafaelxo <rafaelxo2007@gmail.com>
2026-01-27 13:15:01 -0300

    exercicio 64

---
+++ 61-70/exercicio64.cs
@@ -0,0 +1,14 @@
+public class exercicio64 {
+    public int MinPathSum(int[][] grid) {
+        int m = grid.Length, n = grid[0].Length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                else if (i == 0) grid[i][j] += grid[i][j - 1];
+                else if (j == 0) grid[i][j] += grid[i - 1][j];
+                else grid[i][j] += Math.Min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
