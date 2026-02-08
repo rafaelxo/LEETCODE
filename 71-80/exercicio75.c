58fc01eb86491ec1ca4368a8f6e013e277a8650f
rafaelxo <rafaelxo2007@gmail.com>
2026-02-08 11:52:48 -0300

    exercicio 75

---
+++ 71-80/exercicio75.c
@@ -0,0 +1,18 @@
+void sortColors(int* nums, int numsSize) {
+    int low = 0, mid = 0, high = numsSize - 1;
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            int tmp = nums[low];
+            nums[low] = nums[mid];
+            nums[mid] = tmp;
+            low++; mid++;
+        }
+        else if (nums[mid] == 1) mid++;
+        else {
+            int tmp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = tmp;
+            high--;
+        }
+    }
+}
