e6325e9c372355a3dba8e633db98f29aa2115748
rafaelxo <rafaelxo2007@gmail.com>
2026-02-13 10:04:13 -0300

    exercicio 104

--- Easy/exercicio104.java
+++ Easy/exercicio104.java
@@ -1,13 +1,8 @@
 class exercicio104 {
-    public int getHeight(TreeNode a) {
-        if (a == null) return 0;
-        else {
-            int hLeft = getHeight(a.esq);
-            int hRight = getHeight(a.dir);
-            return (hLeft > hRight ? hLeft : hRight) + 1;
-        }
-    }
     public int maxDepth(TreeNode root) {
-        return getHeight(root);
+        if (root== null) return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + Math.max(leftDepth, rightDepth);
     }
 }
