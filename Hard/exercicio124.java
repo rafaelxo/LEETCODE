555a86fe26f5babca134df85107373aa06eff66b
rafaelxo <rafaelxo2007@gmail.com>
2026-03-30 10:34:46 -0300

    exercicio 124

---
+++ Hard/exercicio124.java
@@ -0,0 +1,16 @@
+class Solution {
+    private int sum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return sum;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, dfs(node.left)), right = Math.max(0, dfs(node.right));
+        int curr = node.val + left + right;
+        sum = Math.max(sum, curr);
+        return node.val + Math.max(left, right);
+    }
+}
