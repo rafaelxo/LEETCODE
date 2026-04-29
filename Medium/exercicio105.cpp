ed190dd4b6dc7a8f70c00b38b55f6aa1ce3d710d
rafaelxo <rafaelxo2007@gmail.com>
2026-04-28 21:08:46 -0300

    exercicio 105

---
+++ Medium/exercicio105.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    unordered_map<int, int> inorderIndex;
+    int idx = 0;
+    TreeNode* build(vector<int>& preorder, int left, int right) {
+        if (left > right) return nullptr;
+        int rootVal = preorder[idx++];
+        TreeNode* root = new TreeNode(rootVal);
+        int mid = inorderIndex[rootVal];
+        root->left = build(preorder, left, mid - 1);
+        root->right = build(preorder, mid + 1, right);
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for (int i = 0; i < inorder.size(); i++) inorderIndex[inorder[i]] = i;
+        return build(preorder, 0, inorder.size() - 1);
+    }
+};
