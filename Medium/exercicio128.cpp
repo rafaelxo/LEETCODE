46ef5dc12fb174b785cc03e1cda500d5fee21b8f
rafaelxo <rafaelxo2007@gmail.com>
2026-05-11 19:43:57 -0300

    exercicio 129

---
+++ Medium/exercicio128.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int> &nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        int longest = 0;
+        for (int num : s) {
+            if (!s.count(num - 1)) {
+                int current = num, streak = 1;
+                while (s.count(current + 1)) {
+                    current++;
+                    streak++;
+                }
+                longest = max(longest, streak);
+            }
+        }
+        return longest;
+    }
+};
