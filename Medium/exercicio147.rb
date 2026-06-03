1d051ba06887d87ff058b9b2c85aa85602fb03eb
rafaelxo <rafaelxo2007@gmail.com>
2026-06-03 13:15:56 -0300

    exercicio 147

---
+++ Medium/exercicio147.rb
@@ -0,0 +1,15 @@
+def insertion_sort_list(head)
+  dummy = ListNode.new(0)
+  curr = head
+  while curr
+    next_node = curr.next
+    prev = dummy
+    while prev.next && prev.next.val < curr.val
+      prev = prev.next
+    end
+    curr.next = prev.next
+    prev.next = curr
+    curr = next_node
+  end
+  dummy.next
+end
